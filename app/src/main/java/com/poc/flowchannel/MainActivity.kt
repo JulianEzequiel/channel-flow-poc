484918397fbaf9580ecb8f195796f23692381ea5
Julian Ezequiel Pico <julian.e.pico@gmail.com>
2020-06-01 11:59:11 -0300

    Add StateFlow example
    (cherry picked from commit 6c2a0fdead8c066bba2de416ab386714df9641f0)

--- app/src/main/java/com/poc/flowchannel/MainActivity.kt
+++ app/src/main/java/com/poc/flowchannel/MainActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.poc.flowchannel.app.home.ui.HomeActivity
-import com.poc.flowchannel.examples.ui.BroadcastChannelActivity
-import com.poc.flowchannel.examples.ui.ChannelActivity
-import com.poc.flowchannel.examples.ui.ChannelReceiversActivity
-import com.poc.flowchannel.examples.ui.FlowActivity
+import com.poc.flowchannel.examples.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -25,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         }
         goToChannelButton.setOnClickListener {
             startActivity(Intent(this, ChannelActivity::class.java))
+        }
+        goToStateFlowButton.setOnClickListener {
+            startActivity(Intent(this, StateFlowActivity::class.java))
         }
         goToChannelReceiversButton.setOnClickListener {
             startActivity(Intent(this, ChannelReceiversActivity::class.java))
