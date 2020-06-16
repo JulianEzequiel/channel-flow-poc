0b96a4bb5bad5a1c228fbb7cfd4f9da84c6757c6
Julian Ezequiel Pico <julian.e.pico@gmail.com>
2020-06-16 14:54:56 -0300

    Some clean up
    (cherry picked from commit ad0af9f84f1f35bdb0db5acd0becc004507bda21)

--- app/src/main/java/com/poc/flowchannel/examples/viewmodel/FlowViewModel.kt
+++ app/src/main/java/com/poc/flowchannel/examples/viewmodel/FlowViewModel.kt
@@ -1,33 +1,37 @@
 package com.poc.flowchannel.examples.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.poc.flowchannel.examples.usecase.factory.UseCaseFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 @FlowPreview
 @ExperimentalCoroutinesApi
 class FlowViewModel : ViewModel() {
 
-    private val getFlowUseCase = UseCaseFactory.getFlowUseCase()
-    private val getCallbackFlowUseCase = UseCaseFactory.getCallbackFlowUseCase()
-
     val flowValues = MutableLiveData<String>()
     val callbackFlowValues = MutableLiveData<String>()
+
+    private val flowEmitter: Flow<String> = createFlow()
+    private val callbackFlowEmitter: Flow<String> = createCallbackFlow()
 
     fun initEmissions() {
         observeFlow()
         observeCallbackFlow()
     }
 
-    fun observeFlow() {
+    private fun observeFlow() {
         viewModelScope.launch {
-            getFlowUseCase.execute()
-                .collect {
+            flowEmitter.collect {
                     flowValues.value = it
                 }
         }
@@ -35,10 +39,44 @@ class FlowViewModel : ViewModel() {
 
     private fun observeCallbackFlow() {
         viewModelScope.launch {
-            getCallbackFlowUseCase.execute()
-                .collect {
+            callbackFlowEmitter.collect {
                     callbackFlowValues.value = it
                 }
+        }
+    }
+
+    private fun createFlow(): Flow<String> = flow {
+        var value = 0
+
+        while (true) {
+            emit(value.toString())
+            value++
+            delay(1000)
+        }
+    }
+
+    private fun createCallbackFlow(): Flow<String> = callbackFlow {
+        var coroutine1Value = 0
+        var coroutine2Value = 0
+
+        launch {
+            while (true) {
+                offer("Coroutine1: $coroutine1Value\nCoroutine2: $coroutine2Value")
+                coroutine1Value++
+                delay(1000)
+            }
+        }
+
+        launch {
+            while (true) {
+                offer("Coroutine1: $coroutine1Value\nCoroutine2: $coroutine2Value")
+                coroutine2Value++
+                delay(600)
+            }
+        }
+
+        awaitClose {
+            Log.d("AwaitClose", "Closed")
         }
     }
 
